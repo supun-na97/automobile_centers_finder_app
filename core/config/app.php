e0312e000ad9dd41e61e0defa96eb80bf0f0b0d4
Supun Nanayakkara <43087316+supun-na97@users.noreply.github.com>
2022-08-31 00:12:51 +0530

    Revert "Database table migrations - users (#3)"
    This reverts commit 13c00f3effbf207ca6aef58d5b3b2ad1fb4e4d11.

--- core/config/app.php
+++ core/config/app.php
@@ -161,7 +161,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Jenssegers\Agent\AgentServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -230,7 +229,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Agent' => Jenssegers\Agent\Facades\Agent::class,
 
     ],
 
