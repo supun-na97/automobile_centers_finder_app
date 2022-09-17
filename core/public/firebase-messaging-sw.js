7d5a2e7895b38787b2321ead7223cacb340d7c45
supuna9 <supuna@unionassurance.com>
2022-09-17 18:31:27 +0530

    Firebase configuration and integration

---
+++ core/public/firebase-messaging-sw.js
@@ -0,0 +1,34 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.importScripts('https://www.gstatic.com/firebasejs/7.23.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-messaging.js');
+/*
+Initialize the Firebase app in the service worker by passing in the messagingSenderId.
+*/
+firebase.initializeApp({
+    apiKey: config('services.firebase.api_key'),
+    authDomain: config('services.firebase.auth_domain'),
+    databaseURL: config('services.firebase.db_url'),
+    projectId: config('services.firebase.project_id'),
+    storageBucket: config('services.firebase.storage_bucket'),
+    messagingSenderId: config('services.firebase.sender_id'),
+    appId: config('services.firebase.id'),
+    measurementId: config('services.firebase.measurement_id')
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+messaging.setBackgroundMessageHandler(function(payload) {
+    console.log("Message received.", payload);
+    const title = "Hello world is awesome";
+    const options = {
+        body: "Your notificaiton message .",
+        icon: "/firebase-logo.png",
+    };
+    return self.registration.showNotification(
+        title,
+        options,
+    );
+});
