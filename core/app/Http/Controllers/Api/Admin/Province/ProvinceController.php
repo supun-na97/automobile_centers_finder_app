cfff1e92b6a956d962971f1b9f88f5efbd52f479
supun-na97 <supunnanayakkara01@gmail.com>
2022-09-19 01:17:51 +0530

    Create province related functions

---
+++ core/app/Http/Controllers/Api/Admin/Province/ProvinceController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api\Admin\Province;
+
+use App\Http\Controllers\Api\ApiController;
+use App\Services\Admin\Province\ProvinceService;
+use App\Transformers\Admin\Province\ProvinceTransformer;
+
+class ProvinceController extends ApiController
+{
+    /**
+     * @var provinceService
+     */
+    protected $provinceService;
+
+    /**
+     * Province service.
+     * @param ProvinceService $provinceService
+     */
+    public function __construct(ProvinceService $provinceService)
+    {
+        $this->provinceService = $provinceService;
+    }
+
+    public function province()
+    {
+        $result        = $this->provinceService->getProvince();
+        $data          = $result->unwrap();
+
+        return fractal($data, new ProvinceTransformer)->respond();
+    }
+}
