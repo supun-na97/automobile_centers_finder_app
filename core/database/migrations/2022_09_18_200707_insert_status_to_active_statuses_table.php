05f6808e54d59aecef67e8dcda658a73cb7a0a66
supun-na97 <supunnanayakkara01@gmail.com>
2022-09-19 02:56:15 +0530

    Create active statuses table migration

---
+++ core/database/migrations/2022_09_18_200707_insert_status_to_active_statuses_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class InsertStatusToActiveStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $statuses = [
+            [
+                'name'       => 'Active',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'name'       => 'Offline',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'name'       => 'Busy',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'name'       => 'Close',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]
+        ];
+
+        DB::table('active_statuses')
+            ->insert($statuses);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('active_statuses')
+        ->whereIn('id', [1, 2, 3, 4, 5])
+        ->delete();
+    }
+}
