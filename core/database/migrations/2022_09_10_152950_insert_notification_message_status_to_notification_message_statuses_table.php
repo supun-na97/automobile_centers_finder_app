57b72535da6948fe9f6db9fa86a8e34b5eb85647
supun-na97 <supunnanayakkara01@gmail.com>
2022-09-19 12:46:50 +0530

    Change ids

--- core/database/migrations/2022_09_10_152950_insert_notification_message_status_to_notification_message_statuses_table.php
+++ core/database/migrations/2022_09_10_152950_insert_notification_message_status_to_notification_message_statuses_table.php
@@ -58,7 +58,7 @@ class InsertNotificationMessageStatusToNotificationMessageStatusesTable extends 
     public function down()
     {
         DB::table('notification_message_statuses')
-        ->whereIn('name', ['Ongoing', 'Sending Failed', 'Delivered', 'Reject', 'Sent'])
+        ->whereIn('id', [1, 2, 3, 4, 5])
         ->delete();
     }
 }
