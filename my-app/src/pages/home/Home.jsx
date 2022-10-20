e5203aeaad8393321fc8f183f689322b04a115c1
supun-na97 <supunnanayakkara01@gmail.com>
2022-10-20 22:06:04 +0530

    Remove dashboard body

--- my-app/src/pages/home/Home.jsx
+++ my-app/src/pages/home/Home.jsx
@@ -5,46 +5,7 @@ import {useEffect} from "react";
 const Home = () => {
   return (
     <div className="">
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis fugit perspiciatis, quia rem repudiandae velit. Accusantium alias aliquam blanditiis eum laudantium, magni neque nisi officiis porro veniam veritatis voluptates voluptatum!</div>
-        <div>Home</div>
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis fugit perspiciatis, quia rem repudiandae velit. Accusantium alias aliquam blanditiis eum laudantium, magni neque nisi officiis porro veniam veritatis voluptates voluptatum!</div>
-        <div>Home</div>
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis fugit perspiciatis, quia rem repudiandae velit. Accusantium alias aliquam blanditiis eum laudantium, magni neque nisi officiis porro veniam veritatis voluptates voluptatum!</div>
-        <div>Home</div>
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis fugit perspiciatis, quia rem repudiandae velit. Accusantium alias aliquam blanditiis eum laudantium, magni neque nisi officiis porro veniam veritatis voluptates voluptatum!</div>
-        <div>Home</div>
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis fugit perspiciatis, quia rem repudiandae velit. Accusantium alias aliquam blanditiis eum laudantium, magni neque nisi officiis porro veniam veritatis voluptates voluptatum!</div>
-        <div>Home</div>
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis fugit perspiciatis, quia rem repudiandae velit. Accusantium alias aliquam blanditiis eum laudantium, magni neque nisi officiis porro veniam veritatis voluptates voluptatum!</div>
-        <div>Home</div>
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis fugit perspiciatis, quia rem repudiandae velit. Accusantium alias aliquam blanditiis eum laudantium, magni neque nisi officiis porro veniam veritatis voluptates voluptatum!</div>
-        <div>Home</div>
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis fugit perspiciatis, quia rem repudiandae velit. Accusantium alias aliquam blanditiis eum laudantium, magni neque nisi officiis porro veniam veritatis voluptates voluptatum!</div>
-        <div>Home</div>
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis fugit perspiciatis, quia rem repudiandae velit. Accusantium alias aliquam blanditiis eum laudantium, magni neque nisi officiis porro veniam veritatis voluptates voluptatum!</div>
-        <div>Home</div>
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis fugit perspiciatis, quia rem repudiandae velit. Accusantium alias aliquam blanditiis eum laudantium, magni neque nisi officiis porro veniam veritatis voluptates voluptatum!</div>
-        <div>Home</div>
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis fugit perspiciatis, quia rem repudiandae velit. Accusantium alias aliquam blanditiis eum laudantium, magni neque nisi officiis porro veniam veritatis voluptates voluptatum!</div>
-        <div>Home</div>
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis fugit perspiciatis, quia rem repudiandae velit. Accusantium alias aliquam blanditiis eum laudantium, magni neque nisi officiis porro veniam veritatis voluptates voluptatum!</div>
-        <div>Home</div>
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis fugit perspiciatis, quia rem repudiandae velit. Accusantium alias aliquam blanditiis eum laudantium, magni neque nisi officiis porro veniam veritatis voluptates voluptatum!</div>
-        <div>Home</div>
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis fugit perspiciatis, quia rem repudiandae velit. Accusantium alias aliquam blanditiis eum laudantium, magni neque nisi officiis porro veniam veritatis voluptates voluptatum!</div>
-        <div>Home</div>
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis fugit perspiciatis, quia rem repudiandae velit. Accusantium alias aliquam blanditiis eum laudantium, magni neque nisi officiis porro veniam veritatis voluptates voluptatum!</div>
-        <div>Home</div>
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis fugit perspiciatis, quia rem repudiandae velit. Accusantium alias aliquam blanditiis eum laudantium, magni neque nisi officiis porro veniam veritatis voluptates voluptatum!</div>
-        <div>Home</div>
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis fugit perspiciatis, quia rem repudiandae velit. Accusantium alias aliquam blanditiis eum laudantium, magni neque nisi officiis porro veniam veritatis voluptates voluptatum!</div>
-        <div>Home</div>
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis fugit perspiciatis, quia rem repudiandae velit. Accusantium alias aliquam blanditiis eum laudantium, magni neque nisi officiis porro veniam veritatis voluptates voluptatum!</div>
-        <div>Home</div>
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis fugit perspiciatis, quia rem repudiandae velit. Accusantium alias aliquam blanditiis eum laudantium, magni neque nisi officiis porro veniam veritatis voluptates voluptatum!</div>
-        <div>Home</div>
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis fugit perspiciatis, quia rem repudiandae velit. Accusantium alias aliquam blanditiis eum laudantium, magni neque nisi officiis porro veniam veritatis voluptates voluptatum!</div>
-        <div>Home</div>
+      
     </div>
   )
 }
